2fe7957f4bb582a03853742afed2b0b6d65230f3
Habs Cervantes <rcervantes@exceture.com>
2021-04-16 18:50:36 +0800

    SVC-17 #comment Separated the user info details into a component for easy integration of editing user profile, create integration fo retrieving references needed for profile edit

--- src/app/pages/ProfilePage/index.tsx
+++ src/app/pages/ProfilePage/index.tsx
@@ -5,13 +5,9 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import ProtectedContent from 'app/components/Layouts/ProtectedContent';
 import Box from 'app/components/Box';
-import Button from 'app/components/Elements/Button';
-import IconButton from 'app/components/Elements/IconButton';
-import List from 'app/components/List';
-import ListItem from 'app/components/List/ListItem';
-import ListItemText from 'app/components/List/ListItemText';
 
 import UserInfo from './UserInfo';
+import UserInfoList from './UserDetails';
 
 export function UserProfilePage() {
   return (
@@ -20,135 +16,10 @@ export function UserProfilePage() {
         <title>Profile</title>
       </Helmet>
 
-      <Box
-        title="User Profile"
-        titleBorder
-        footerBorder
-        footer={
-          <Button color="secondary" size="medium">
-            Add Money
-          </Button>
-        }
-        footerAlign="center"
-      >
+      <Box title="User Profile" titleBorder>
         <div style={{ padding: '20px 25px' }}>
-          <UserInfo profile />
-          <List divider>
-            <ListItem flex>
-              <ListItemText
-                primary="Change Email Address"
-                style={{
-                  flexGrow: 1,
-                }}
-              />
-              <IconButton onClick={() => alert('clicked')}>
-                <FontAwesomeIcon icon="chevron-right" />
-              </IconButton>
-            </ListItem>
-            <ListItem flex>
-              <ListItemText
-                primary="Change Mobile Number"
-                style={{
-                  flexGrow: 1,
-                }}
-              />
-              <IconButton onClick={() => alert('clicked')}>
-                <FontAwesomeIcon icon="chevron-right" />
-              </IconButton>
-            </ListItem>
-            <ListItem flex>
-              <ListItemText
-                primary="Upload ID"
-                style={{
-                  flexGrow: 1,
-                }}
-              />
-              <IconButton onClick={() => alert('clicked')}>
-                <FontAwesomeIcon icon="chevron-right" />
-              </IconButton>
-            </ListItem>
-            <ListItem flex>
-              <ListItemText
-                label="Birthdate"
-                primary="02/17/1995"
-                style={{
-                  flexGrow: 1,
-                }}
-              />
-            </ListItem>
-            <ListItem flex>
-              <ListItemText
-                label="Birthplace"
-                primary="East Avenue Medical Center"
-                style={{
-                  flexGrow: 1,
-                }}
-              />
-            </ListItem>
-            <ListItem flex>
-              <ListItemText
-                label="Country"
-                primary="Philippines"
-                style={{
-                  flexGrow: 1,
-                }}
-              />
-            </ListItem>
-            <ListItem flex>
-              <ListItemText
-                label="Address"
-                primary="11 De Castro Avenue, De Castro Subdivision, Sta. Lucia 1600"
-                style={{
-                  flexGrow: 1,
-                }}
-              />
-            </ListItem>
-            <ListItem flex>
-              <ListItemText
-                label="Province / State"
-                primary="Bohol"
-                style={{
-                  flexGrow: 1,
-                }}
-              />
-            </ListItem>
-            <ListItem flex>
-              <ListItemText
-                label="City"
-                primary="Quezon City"
-                style={{
-                  flexGrow: 1,
-                }}
-              />
-            </ListItem>
-            <ListItem flex>
-              <ListItemText
-                label="Postal Code"
-                primary="1101"
-                style={{
-                  flexGrow: 1,
-                }}
-              />
-            </ListItem>
-            <ListItem flex>
-              <ListItemText
-                label="Source of Funds"
-                primary="Work"
-                style={{
-                  flexGrow: 1,
-                }}
-              />
-            </ListItem>
-            <ListItem flex>
-              <ListItemText
-                label="Nature of Work"
-                primary="Office"
-                style={{
-                  flexGrow: 1,
-                }}
-              />
-            </ListItem>
-          </List>
+          <UserInfo profile onEdit={() => alert('edit')} />
+          <UserInfoList />
         </div>
       </Box>
     </ProtectedContent>
