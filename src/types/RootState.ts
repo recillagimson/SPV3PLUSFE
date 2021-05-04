9fbd46c3fa671b6830a2e4508dd02867a62b76f1
Robi <robbietuazon@gmail.com>
2021-05-04 18:20:07 +0800

    merge

--- src/types/RootState.ts
+++ src/types/RootState.ts
@@ -7,12 +7,9 @@ import { RegisterState } from 'app/pages/RegisterPage/slice/types';
 import { ForgotPasswordState } from 'app/pages/ForgotPasswordPage/slice/types';
 import { SendMoneyState } from 'app/pages/SendMoney/slice/types';
 import { ProfileState } from 'app/pages/ProfilePage/slice/types';
-<<<<<<< HEAD
 import { NotificationsState } from 'app/pages/Notification/slice/types';
-=======
 import { ChangePasswordState } from 'app/pages/SettingsPage/ChangePassword/slice/types';
 import { ChangePinState } from 'app/pages/SettingsPage/ChangePin/slice/types';
->>>>>>> 5afa8a0c3ca57203bb3fa14be7cb9585c5ff54bf
 
 // components
 import { UpdatePasswordState } from 'app/components/UpdatePassword/slice/types';
@@ -32,10 +29,7 @@ export interface RootState {
   sendMoney?: SendMoneyState;
   profile?: ProfileState;
   notifications?: NotificationsState;
-
   helpCenter?: any;
-
-
   changePassword?: ChangePasswordState;
   changePin?: ChangePinState;
   // [INSERT NEW REDUCER KEY ABOVE] < Needed for generating containers seamlessly
