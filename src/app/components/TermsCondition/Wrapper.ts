3be8e72a2eb50a5184e261e5d53f79817035bc90
Habs Cervantes <rcervantes@exceture.com>
2021-09-13 18:56:10 +0800

    Updated privacy and terms pages

---
+++ src/app/components/TermsCondition/Wrapper.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components/macro';
+
+const Wrapper = styled.div`
+  padding: 30px 25px;
+
+  h4 {
+    font-size: 1.5rem;
+    margin: 0 0 20px;
+  }
+
+  h5 {
+    font-size: 1.125rem;
+    margin: 0 0 20px;
+  }
+
+  p {
+    margin: 0 0 20px;
+  }
+
+  ul.letter-list {
+    list-style: lower-alpha;
+  }
+`;
+
+export default Wrapper;
